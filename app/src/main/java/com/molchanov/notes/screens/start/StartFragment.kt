2c177d0378377e57d38e7ee83988bd1b9da439c3
Yaroslav <tsar.leonid1@gmail.com>
2021-01-22 11:20:56 +0300

    Update dependencies

--- app/src/main/java/com/molchanov/notes/screens/start/StartFragment.kt
+++ app/src/main/java/com/molchanov/notes/screens/start/StartFragment.kt
@@ -13,18 +13,19 @@ import com.molchanov.notes.utils.*
 
 class StartFragment : Fragment() {
 
-    private var _binding : FragmentStartBinding? = null
+    private var _binding: FragmentStartBinding? = null
     private val mBinding get() = _binding!!
     private lateinit var mViewModel: StartFragmentViewModel
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
-            return mBinding.root
+        return mBinding.root
 
     }
 
@@ -42,9 +43,10 @@ class StartFragment : Fragment() {
         }
 
     }
+
     private fun initialization() {
 
-        mBinding.btnRoom.setOnClickListener{
+        mBinding.btnRoom.setOnClickListener {
             mViewModel.initDatabase(TYPE_ROOM) {
                 AppPreference.setInitUser(true)
                 AppPreference.setTypeDB(TYPE_ROOM)
@@ -52,34 +54,34 @@ class StartFragment : Fragment() {
             }
         }
         //Подключаем слушатель к кнопке файербэйс
-        mBinding.btnFirebase.setOnClickListener{
+        mBinding.btnFirebase.setOnClickListener {
             //При нажатии переводим поля ввода почты, пароля и кнопку логин в режим видимости
-               mBinding.let {
-                   it.etInputEmail.visibility = View.VISIBLE
-                   it.etInputPassword.visibility = View.VISIBLE
-                   it.btnLogin.apply{
-                       visibility = View.VISIBLE
-                       //подключаем слушатель на кнопку логин
-                       setOnClickListener {
-                           //создаем переменные с текстом из ввода почты и пароля и проверяем
-                           //не пустые ли они. Если пустые - выводим тост
-                           val inputEmail = mBinding.etInputEmail.text.toString()
-                           val inputPassword = mBinding.etInputPassword.text.toString()
-                           if (inputEmail.isNotEmpty() && inputPassword.isNotEmpty()) {
-                               EMAIL = inputEmail
-                               PASSWORD = inputPassword
-                               mViewModel.initDatabase(TYPE_FIREBASE) {
-                                   AppPreference.setInitUser(true)
-                                   AppPreference.setTypeDB(TYPE_FIREBASE)
-                                   APP_ACTIVITY.navController.navigate(R.id.action_startFragment_to_mainFragment)
-                               }
-                           } else {
-                               showToast(context.getString(R.string.toast_wrong_login_data))
-                           }
-                       }
-                   }
+            mBinding.let {
+                it.etInputEmail.visibility = View.VISIBLE
+                it.etInputPassword.visibility = View.VISIBLE
+                it.btnLogin.apply {
+                    visibility = View.VISIBLE
+                    //подключаем слушатель на кнопку логин
+                    setOnClickListener {
+                        //создаем переменные с текстом из ввода почты и пароля и проверяем
+                        //не пустые ли они. Если пустые - выводим тост
+                        val inputEmail = mBinding.etInputEmail.text.toString()
+                        val inputPassword = mBinding.etInputPassword.text.toString()
+                        if (inputEmail.isNotEmpty() && inputPassword.isNotEmpty()) {
+                            EMAIL = inputEmail
+                            PASSWORD = inputPassword
+                            mViewModel.initDatabase(TYPE_FIREBASE) {
+                                AppPreference.setInitUser(true)
+                                AppPreference.setTypeDB(TYPE_FIREBASE)
+                                APP_ACTIVITY.navController.navigate(R.id.action_startFragment_to_mainFragment)
+                            }
+                        } else {
+                            showToast(context.getString(R.string.toast_wrong_login_data))
+                        }
+                    }
+                }
 
-               }
+            }
         }
     }
 
